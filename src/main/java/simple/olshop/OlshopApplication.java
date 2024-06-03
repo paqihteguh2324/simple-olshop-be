16aee67c2739d32d715ed32a2550fb430a4ee39b
Paqihteguh2324 <Paqihteguh23@gmail.com>
2024-06-03 21:47:30 +0700

    initial commit

---
+++ src/main/java/simple/olshop/OlshopApplication.java
@@ -0,0 +1,13 @@
+package simple.olshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simple/olshop/OlshopApplicationTests.java
@@ -0,0 +1,13 @@
+package simple.olshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
